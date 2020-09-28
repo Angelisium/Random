9a0c551ec56fecece0f0ca635467f6821a857ad6
Angelisium <eliard.alexis@gmail.com>
2020-09-28 19:17:52 +0200

    Update test.js
    test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,60 @@
+const pre = document.querySelector('pre');
+function addTextNode(str) {
+    let a = document.createTextNode(str);
+    pre.appendChild(a);
+}
+function addTextsNode(carray) {
+    for(let a in carray) {
+        addTextNode(`${a}: ${carray[a]}\n`);
+    }
+}
+
+const min = 0;
+const max = 10;
+
+var timeseed = Date.now(),
+    rand = alea("myCustomSeed"+timeseed),
+    tab = [];
+
+var start = Date.now();
+for(let a=0; a<999999; a++) {
+    let x = ((rand() * (max - min)) | 0) + min;
+    if(!tab[x]) tab[x] = 0;
+    tab[x]++;
+}
+var end = Date.now();
+
+addTextNode(`Alea()\n`);
+addTextsNode(tab);
+addTextNode(`Execute in ${end-start}ms (${end}) (${start})\n\n`);
+
+var tab = [];
+var start = Date.now();
+for(let a=0; a<999999; a++) {
+    let x = ((Math.random() * (max - min)) | 0) + min;
+    if(!tab[x]) tab[x] = 0;
+    tab[x]++;
+}
+var end = Date.now();
+addTextNode(`Math.random()\n`);
+addTextsNode(tab);
+addTextNode(`Execute in ${end-start}ms (${end}) (${start})\n\n`);
+
+var rand = new Random("myCustomSeed"+timeseed),
+    tab = [];
+
+var start = Date.now();
+for(let a=0; a<999999; a++) {
+    let x = rand.toRange(min, max);
+    if(!tab[x]) tab[x] = 0;
+    tab[x]++;
+}
+var end = Date.now();
+addTextNode(`Random()\n`);
+addTextsNode(tab);
+addTextNode(`Execute in ${end-start}ms (${end}) (${start})\n\n`);
+
+/*
 var alea_test_vid = alea(),
     alea_test_str = alea("seed_test"),
     alea_test_int = alea(215),
@@ -8,11 +65,7 @@ var rand_test_vid = new Random("undefined"), // for work like alea()
     rand_test_int = new Random(215),
     rand_test_bint = new Random(42n);
 
-const pre = document.querySelector('pre');
-addTextNode = function(str) {
-    let z = document.createTextNode(str);
-    pre.appendChild(z);
-}
+
 
 for(let i=0; i<10; i++) {
     addTextNode(`Alea {}: ${alea_test_vid()} \t\t\t random {}: ${rand_test_vid.next()}\n`);
@@ -45,4 +98,4 @@ addTextNode(`${randTab.toString()} \n`);
 var randrange = new Random("ca8+94dx1zad8xa4z6");
 for(let i=0; i<20; i++) {
     addTextNode(`${randrange.toFloatRange(0.5,5.5)} \t`);
-}
+} */
